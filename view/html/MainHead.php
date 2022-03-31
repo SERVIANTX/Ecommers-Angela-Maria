f957b3fdd9c1d046b34b974e57b14e55548619a6
Serviantx <eduardoas0223@gmail.com>
2022-03-31 17:13:12 -0500

    CREACION DEL TEMPLATE

---
+++ view/html/MainHead.php
@@ -0,0 +1,13 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Vendors Min CSS -->
+<link rel="stylesheet" href="../../assets/css/vendors.min.css">
+<!-- Style CSS -->
+<link rel="stylesheet" href="../../assets/css/style.css">
+<!-- Responsive CSS -->
+<link rel="stylesheet" href="../../assets/css/responsive.css">
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
+
+<link rel="stylesheet" href="../../assets/css/select2.min.css">
---
+++ view/html/MainNavbar.php
@@ -0,0 +1,106 @@
+<nav class="navbar top-navbar navbar-expand">
+    <div class="collapse navbar-collapse" id="navbarSupportContent">
+        <div class="responsive-burger-menu d-block d-lg-none">
+            <span class="top-bar"></span>
+            <span class="middle-bar"></span>
+            <span class="bottom-bar"></span>
+        </div>
+
+        <form class="nav-search-form d-none ml-auto d-md-block">
+
+        </form>
+
+        <ul class="navbar-nav right-nav align-items-center">
+            <li class="nav-item">
+                <a class="nav-link bx-fullscreen-btn" id="fullscreen-button">
+                    <i class="bx bx-fullscreen"></i>
+                </a>
+            </li>
+
+            <li class="nav-item dropdown language-switch-nav-item">
+                <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <img src="../../assets/img/spain-flag.jpg" alt="image">
+                    <span>Español <i class='bx bx-chevron-down'></i></span>
+                </a>
+
+                <div class="dropdown-menu">
+                    <a href="#" class="dropdown-item d-flex justify-content-between align-items-center">
+                        <span>German</span>
+
+                        <img src="../../assets/img/germany-flag.jpg" alt="flag">
+                    </a>
+
+                    <a href="#" class="dropdown-item d-flex justify-content-between align-items-center">
+                        <span>French</span>
+
+                        <img src="../../assets/img/france-flag.jpg" alt="flag">
+                    </a>
+
+                    <a href="#" class="dropdown-item d-flex justify-content-between align-items-center">
+                        <span>English</span>
+
+                        <img src="../../assets/img/us-flag.jpg" alt="flag">
+                    </a>
+
+                    <a href="#" class="dropdown-item d-flex justify-content-between align-items-center">
+                        <span>Russian</span>
+
+                        <img src="../../assets/img/russia-flag.jpg" alt="flag">
+                    </a>
+
+                    <a href="#" class="dropdown-item d-flex justify-content-between align-items-center">
+                        <span>Italian</span>
+
+                        <img src="../../assets/img/italy-flag.jpg" alt="flag">
+                    </a>
+                </div>
+            </li>
+
+
+            <li class="nav-item dropdown profile-nav-item">
+                <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <div class="menu-profile">
+                        <span class="name">Hola Admin!</span>
+                        <img src="../../assets/img/user1.jpg" class="rounded-circle" alt="image">
+                    </div>
+                </a>
+
+                <div class="dropdown-menu">
+                    <div class="dropdown-header d-flex flex-column align-items-center">
+                        <div class="figure mb-3">
+                            <img src="../../assets/img/user1.jpg" class="rounded-circle" alt="image">
+                        </div>
+
+                        <div class="info text-center">
+                            <span class="name">Admin</span>
+                            <p class="mb-3 email">admin@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div class="dropdown-body">
+                        <ul class="profile-nav p-0 pt-3">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class='bx bx-user'></i> <span>Perfil</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <div class="dropdown-footer">
+                        <ul class="profile-nav">
+                            <li class="nav-item">
+                                <a href="../html/logout.php" class="nav-link">
+                                    <i class='bx bx-log-out'></i> <span>Cerrar Sesión</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
