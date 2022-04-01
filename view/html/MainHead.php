04c7ca393c063f374bc4675ff6ab5a616cd5d0af
Jose-FD <jf153413@gmail.com>
2022-04-01 13:07:21 -0500

    ACTUALIZACION DEL DISEÑO DEL ECOMMERCE

--- view/html/MainHead.php
+++ view/html/MainHead.php
@@ -1,13 +1,25 @@
-<!-- Required meta tags -->
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!-- Vendors Min CSS -->
-<link rel="stylesheet" href="../../assets/css/vendors.min.css">
-<!-- Style CSS -->
-<link rel="stylesheet" href="../../assets/css/style.css">
-<!-- Responsive CSS -->
-<link rel="stylesheet" href="../../assets/css/responsive.css">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
+<title>Electro - HTML Ecommerce Template</title>
 
-<link rel="stylesheet" href="../../assets/css/select2.min.css">
+<!-- Google font -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+<!-- Bootstrap -->
+<link type="text/css" rel="stylesheet" href="../../assets/css/bootstrap.min.css" />
+
+<!-- Slick -->
+<link type="text/css" rel="stylesheet" href="../../assets/css/slick.css" />
+<link type="text/css" rel="stylesheet" href="../../assets/css/slick-theme.css" />
+
+<!-- nouislider -->
+<link type="text/css" rel="stylesheet" href="../../assets/css/nouislider.min.css" />
+
+<!-- Font Awesome Icon -->
+<link rel="stylesheet" href="../../assets/css/font-awesome.min.css">
+
+<!-- Custom stlylesheet -->
+<link type="text/css" rel="stylesheet" href="../../assets/css/style.css" />
